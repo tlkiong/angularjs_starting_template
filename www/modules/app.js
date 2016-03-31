0bf15c0b41f6af4d5c1cdb959b71e8c374c3e5cf
Kiong Kiong <kiong90@gmail.com>
2016-04-01 05:53:06 +0800

    Added session control by auth in app.js

--- www/modules/app.js
+++ www/modules/app.js
@@ -8,7 +8,9 @@
         .config(function($locationProvider, $stateProvider, $urlRouterProvider, $compileProvider) {
             $urlRouterProvider.otherwise('/');
             $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile|content):|data:image\//);
-
+            // Uncomment the below line when deploying. This will remove all errors that is shown in console.log
+            // $compileProvider.debugInfoEnabled(false);
+            // 
             // use the HTML5 History API
             // Also known as remove the '#' from the URL
             $locationProvider.html5Mode({
@@ -25,13 +27,53 @@
             // $translateProvider.useSanitizeValueStrategy('sanitize'); // Refer http://angular-translate.github.io/docs/#/guide/19_security for security on Angular-translate
             /* ==================================== End: Translation ==================================== */
 
-        }).run(function(/* $state */$rootScope) {
-        	$rootScope.$on('$stateChangeStart', function(evnt, toState, toParams, fromState, fromParams) {
-		        // If you want to stop the state change event, use:
-		        // 		evnt.preventDefault();
-		        // If you want to change state:
-		        // 		evnt.preventDefault();
-		        // 		$state.go('statename');		<= Do remember to add $state as dependency on 'run' as shown above
-		    });
+        }).run(function(sessionService, commonService,$state, $rootScope) {
+        	var cmnSvc = commonService;
+            var sessionSvc = sessionService;
+
+            $rootScope.$on('$stateChangeStart', function(evnt, toState, toParams, fromState, fromParams) {
+                if(cmnSvc.isObjPresent(toState.role) && cmnSvc.isObjPresent(sessionSvc.userData) && cmnSvc.isObjPresent(sessionSvc.userData.role)) {
+                    if(toState.url === '/') {
+                        if(sessionSvc.isUserLoggedIn() === true) {
+                            evnt.preventDefault();
+                            $state.go('dashboard');
+                        } else {
+                            evnt.preventDefault();
+                            $state.go('home');
+                        }
+                    } else {
+                        if((toState.role.toLowerCase != 'public')){
+                            if(sessionSvc.isUserLoggedIn() != true) {
+                                evnt.preventDefault();
+                                $state.go('login');
+                            } else {
+                                if(hasNoAccess(toState.role)) {
+                                    // Show unauthorized page
+                                    alert('You are unauthorized to enter here');
+                                    evnt.preventDefault();
+                                    $state.go('login');
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            function hasNoAccess(roleList) {
+                if(cmnSvc.getObjType(roleList) === 'array') {
+                    for(var i=0,j=roleList.length; i<j; i++) {
+                        if(roleList[i].toLowerCase() == sessionSvc.userData.role.toLowerCase()) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } else if (cmnSvc.getObjType(roleList) === 'string') {
+                    if(roleList.toLowerCase() == sessionSvc.userData.role.toLowerCase()) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }
         });
 })()
