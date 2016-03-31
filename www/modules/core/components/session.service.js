43be95a67e728d138a19d0ebe43f326776b84c35
Kiong Kiong <kiong90@gmail.com>
2016-04-01 05:58:39 +0800

    Added save, load & remove session

--- www/modules/core/components/session.service.js
+++ www/modules/core/components/session.service.js
@@ -24,6 +24,42 @@
         }
 
         /* ======================================== Private Methods ======================================== */
+        function loadSession(callback) {
+            if(cmnSvc.isObjPresent(service.userData)) {
+                for(var key in service.userData) {
+                    if(service.userData.hasOwnProperty(key)) {
+                        service.userData[key] = localStorage.getItem(key);
+                    }
+                }
+                if(cmnSvc.isObjPresent(service.userData)) {
+                    callback(true);
+                } else {
+                    callback(undefined);
+                }
+            }
+            callback(undefined);
+        }
+
+        function removeSession() {
+            if(cmnSvc.isObjPresent(service.userData)) {
+                for(var key in service.userData) {
+                    if(service.userData.hasOwnProperty(key)) {
+                        localStorage.removeItem(key);
+                    }
+                }
+            }
+        }
+
+        function saveSession() {
+            if(cmnSvc.isObjPresent(service.userData)) {
+                for(var key in service.userData) {
+                    if(service.userData.hasOwnProperty(key)) {
+                        localStorage.setItem(key, service.userData[key]);
+                    }
+                }
+            }
+        }
+        
         function init() {
             
         }
