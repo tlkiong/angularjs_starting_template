b4e243383f7f47367d16474d095bf48209fcdc60
Kiong <kiong90@gmail.com>
2016-10-18 10:21:53 +0800

    Fix indentaion on sample.js

--- www/modules/sample/sample.js
+++ www/modules/sample/sample.js
@@ -1,37 +1,37 @@
 (function () {
-    'use strict';
+  'use strict';
     
-	angular.module('Sample', [])
-		.config(/*@ngInject*/function ($stateProvider) {
-			$stateProvider
-				.state('sample', {
-                    url: '/',
-                    views: {
-                        'main': {
-                            templateUrl: './modules/sample/sample.html',
-                            controller: 'sampleController',
-                            controllerAs: 'vm'
-                        }
-                    }
-                });
-                /** The below shows how to load a view within 'root.html'
-                 *  This is because this will be a child state of root.html
-                 *  params is the way you can pass data around the state.
-                 *      - To pass the data: $state.go('root.sample', {data: 'lala'});      <= This is handled in commonService.goToPage(...)
-                 *      - To fetch the data passed to sampleController: $stateParams       <= This is handled in commonService.getStateParam()
-                 */
-                // .state('root.sample', {
-                //     url: 'sample',       <= the url here will be: /root/sample
-                //     params: {
-                //         data: null,
-                //     },
-                //     views: {
-                //         'subMain': {     <= This means load this bottom view into the subMain named ui-view
-                //             templateUrl: './modules/sample/sample.html',
-                //             controller: 'sampleController',
-                //             controllerAs: 'vm'
-                //         }
-                //     }
-                // });
-		})
+  angular.module('Sample', [])
+    .config(/*@ngInject*/function ($stateProvider) {
+      $stateProvider
+        .state('sample', {
+            url: '/',
+            views: {
+                'main': {
+                    templateUrl: './modules/sample/sample.html',
+                    controller: 'sampleController',
+                    controllerAs: 'vm'
+                }
+            }
+        });
+        /** The below shows how to load a view within 'root.html'
+         *  This is because this will be a child state of root.html
+         *  params is the way you can pass data around the state.
+         *      - To pass the data: $state.go('root.sample', {data: 'lala'});      <= This is handled in commonService.goToPage(...)
+         *      - To fetch the data passed to sampleController: $stateParams       <= This is handled in commonService.getStateParam()
+         */
+        // .state('root.sample', {
+        //     url: 'sample',       <= the url here will be: /root/sample
+        //     params: {
+        //         data: null,
+        //     },
+        //     views: {
+        //         'subMain': {     <= This means load this bottom view into the subMain named ui-view
+        //             templateUrl: './modules/sample/sample.html',
+        //             controller: 'sampleController',
+        //             controllerAs: 'vm'
+        //         }
+        //     }
+        // });
+    })
 })();
