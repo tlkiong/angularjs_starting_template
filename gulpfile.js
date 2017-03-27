31b97331dc7152ae1c7f65385f2d6aa17f5ae243
Kiong <kiong90@gmail.com>
2017-03-27 16:10:39 +0800

    core: Add gulp tasks to create prod files

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,11 @@ var watch = require('gulp-watch');
 var mainBowerFiles = require('main-bower-files');
 var merge = require('merge-stream');
 var runSequence = require('run-sequence');
+var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var bowerFiles = require('bower-files')();
 
 // ============== Convert this to a gulp task to create a gulp task - input, doc's title ==============
 
@@ -20,12 +25,12 @@ gulp.task('ngdocs', [], function() {
     var src = './docs/';
 
     gulp.watch('www/modules/**/*.js', function() {
-        return gulp.src('www/modules/**/*.js')
-            .pipe(gulpDocs.process({
-                html5Mode: false,
-                title: 'AngularJS Dropdown Directive'
-            }))
-            .pipe(gulp.dest(src));
+      return gulp.src('www/modules/**/*.js')
+        .pipe(gulpDocs.process({
+          html5Mode: false,
+          title: 'AngularJS Dropdown Directive'
+        }))
+        .pipe(gulp.dest(src));
     });
 
     connect.server({
@@ -45,39 +50,66 @@ gulp.task('ngdocs', [], function() {
 // To run initial setup synchronously
 gulp.task('setup', function() {
     runSequence('bower',
-        'generateView',
-        'sass',
-        'inject');
+        'generateDevView',
+        'sass-dev',
+        'injectDev');
 });
 
 // For dev purpose. Must have bower & npm install done previously
 gulp.task('dev', function() {
-    runSequence('generateView',
-        'sass',
-        'inject');
+    runSequence('generateDevView',
+        'sass-dev',
+        'injectDev');
+    // load live reload server
+});
+
+// For prod purpose. Must have bower & npm install done previously
+gulp.task('prod', function() {
+    runSequence('generateProdView',
+        'sass-prod',
+        'injectProd');
     // load live reload server
 });
 
 gulp.task('update', function() {
     runSequence('bower',
-        'generateView',
-        'sass',
-        'inject');
+        'generateDevView',
+        'sass-dev',
+        'injectDev');
 });
 
 // Inject bower components, angular components (module) and css
-gulp.task('inject', function() {
-    runSequence('inject-bower',
-        'inject-angular',
-        'inject-css');
+gulp.task('injectDev', function() {
+    runSequence('inject-dev-bower',
+        'inject-dev-angular',
+        'inject-dev-css');
+});
+
+// Inject bower components, angular components (module) and css
+gulp.task('injectProd', function() {
+    runSequence('inject-prod-bower',
+        'inject-prod-angular',
+        'inject-prod-css');
 });
 
 gulp.task('watch', ['watchSass']);
 
 // Compile sass
-gulp.task('sass', function() {
+gulp.task('sass-dev', function() {
     gulp.src('./www/modules/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(rename({ extname: '.css' }))
+        .pipe(gulp.dest('./www/modules/'));
+});
+
+// Compile sass
+gulp.task('sass-prod', function() {
+    gulp.src('./www/modules/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss({
+          keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest('./www/modules/'));
 });
 
@@ -91,7 +123,7 @@ gulp.task('bower', function() {
     });
 });
 
-gulp.task('generateView', function() {
+gulp.task('generateDevView', function() {
     // createIndexHtml stream
     var createIndexHtml = gulp.src('./www/main-view/pre-index.html')
         .pipe(rename('index.html'))
@@ -107,7 +139,49 @@ gulp.task('generateView', function() {
     return merge(compileModuleSass, createIndexHtml);
 });
 
-gulp.task('inject-bower', function() {
+gulp.task('generateProdView', function() {
+    // createIndexHtml stream
+    var createIndexHtml = gulp.src('./www/main-view/pre-index.html')
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./dist/'));
+    // End of createIndexHtml stream
+
+    // compileModuleSass stream
+    var compileModuleSass = gulp.src('./www/modules/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss({
+          keepSpecialComments: 0
+        }))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('./dist/'));
+    // End of compileModuleSass stream
+    
+    var compileBowerCss = gulp.src(bowerFiles.ext('css').files)
+        .pipe(minifyCss({
+          keepSpecialComments: 0
+        }))
+        .pipe(concat('bower.min.css'))
+        .pipe(gulp.dest('./dist/'));
+
+    var compileBowerJs = gulp.src(bowerFiles.ext('js').files)
+        .pipe(concat('bower.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
+
+    var compileModuleAngular = gulp.src(['./www/modules/**/*.js'])
+        .pipe(angularFilesort())
+        .pipe(concat('app.min.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
+
+    var moveAllViews = gulp.src(['./www/modules/**/*.html'])
+        .pipe(gulp.dest('./dist/modules/'))
+
+    return merge(compileModuleSass, compileBowerCss, compileBowerJs, compileModuleAngular, moveAllViews, createIndexHtml);
+});
+
+gulp.task('inject-dev-bower', function() {
     var bowerOptions = {
         paths: {
             bowerDirectory: './www/lib',
@@ -129,7 +203,19 @@ gulp.task('inject-bower', function() {
         .pipe(gulp.dest('./www/'));
 });
 
-gulp.task('inject-angular', function() {
+gulp.task('inject-prod-bower', function() {
+    var target = gulp.src('./dist/index.html');
+    var sources = gulp.src(['./dist/bower.min.js', './dist/bower.min.css']);                        
+
+    return target
+        .pipe(inject(sources, {
+            name: 'bower',
+            relative: 'true'
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('inject-dev-angular', function() {
     var target = gulp.src('./www/index.html');
     var sources = gulp.src(['./www/modules/**/*.js']).pipe(angularFilesort());
 
@@ -141,7 +227,19 @@ gulp.task('inject-angular', function() {
         .pipe(gulp.dest('./www/'));
 });
 
-gulp.task('inject-css', function() {
+gulp.task('inject-prod-angular', function() {
+    var target = gulp.src('./dist/index.html');
+    var sources = gulp.src(['./dist/app.min.js']);                        
+
+    return target
+        .pipe(inject(sources, {
+            name: 'angular',
+            relative: 'true'
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('inject-dev-css', function() {
     var target = gulp.src('./www/index.html');
     var sources = gulp.src(['./www/modules/**/*.css']);
 
@@ -150,4 +248,15 @@ gulp.task('inject-css', function() {
             relative: 'true'
         }))
         .pipe(gulp.dest('./www/'));
+});
+
+gulp.task('inject-prod-css', function() {
+    var target = gulp.src('./dist/index.html');
+    var sources = gulp.src(['./dist/app.min.css']);
+
+    return target
+        .pipe(inject(sources, {
+            relative: 'true'
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
